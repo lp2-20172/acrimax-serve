7f81936d643eb23232e67657e223bc00d8115658
leayobana <leaquillx@gmail.com>
2017-11-08 18:35:43 -0500

    modific

--- ecomm_main/settings.py
+++ ecomm_main/settings.py
@@ -86,19 +86,19 @@ WSGI_APPLICATION = 'ecomm_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASESx = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASESx = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd1ncp92b7bmtnk',
-        'USER': 'mwjmtvwapsnckd',
-        'PASSWORD': '248ee5ab24bf67708000c49b59a66506d3b5d863f2358df8d5f3736bc9c5a2db',
-        'HOST': 'ec2-50-16-228-232.compute-1.amazonaws.com',
+        'NAME': 'da3m0oaeskg00e',
+        'USER': 'kpowpthfsqxvnm',
+        'PASSWORD': '49a44bca1796c8b3cf7f2c18b658a30c02085be4f96a213f1099357deaf8f034',
+        'HOST': 'ec2-107-22-162-82.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
